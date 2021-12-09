d3c0d75a4de67259b9bb84e308b8760ec7f32fb3
KirillSmirnovff <smirnovk.1990@gmail.com>
2021-12-09 13:59:32 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/linkedlist/SimpleLinkedList.java
@@ -0,0 +1,79 @@
+package ru.job4j.linkedlist;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements List<E> {
+
+    private Node<E> first;
+    private Node<E> last;
+    private int size = 0;
+    private int modCount = 0;
+
+    private static class Node<E> {
+
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+    @Override
+    public void add(E value) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, value, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> result = first;
+        if (index != 0) {
+            for (int i = 1; i <= index; i++) {
+                result = result.next;
+            }
+        }
+        return result.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+
+            private Node<E> cursor = first;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return cursor != null;
+            }
+
+            @Override
+            public E next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E result = cursor.item;
+                cursor = cursor.next;
+                return result;
+            }
+        };
+    }
+}
