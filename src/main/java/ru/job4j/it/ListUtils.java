c887704d016534bf93b14f69e0453baf7174a098
KirillSmirnovff <smirnovk.1990@gmail.com>
2021-12-15 13:58:17 +0300

    7. ListIterator [#350217]

---
+++ src/main/java/ru/job4j/it/ListUtils.java
@@ -0,0 +1,68 @@
+package ru.job4j.it;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class ListUtils {
+
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (i.nextIndex() == index) {
+                i.add(value);
+                break;
+            }
+            i.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> i = list.listIterator();
+        for (int j = 0; j < list.size(); j++) {
+            i.next();
+            if (j == index) {
+                i.add(value);
+                break;
+            }
+        }
+
+    }
+
+    public static <T> void removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> i = list.listIterator();
+        if (list.isEmpty()) {
+            throw new IllegalStateException();
+        }
+        do {
+            if (filter.test(i.next())) {
+                i.remove();
+            }
+        } while (i.hasNext());
+    }
+
+    public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> i = list.listIterator();
+        if (list.isEmpty()) {
+            throw new IllegalStateException();
+        }
+        do {
+            if (filter.test(i.next())) {
+                i.set(value);
+            }
+        } while (i.hasNext());
+    }
+
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> i = list.listIterator();
+        if (list.isEmpty()) {
+            throw new IllegalStateException();
+        }
+        do {
+            if (elements.contains(i.next())) {
+                i.remove();
+            }
+        } while (i.hasNext());
+    }
+}
