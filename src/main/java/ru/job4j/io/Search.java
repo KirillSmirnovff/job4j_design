4ba25739c7a9b06e58835f92f57039972cdb40a4
KirillSmirnovff <smirnovk.1990@gmail.com>
2022-01-19 13:49:04 +0300

    5. Валидация параметров запуска. [#246865 #251195]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,18 +9,18 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder and search condition is null");
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Need to set 2 arguments in that order: root folder and file extension");
         }
-        if (args[0] == null) {
-            throw new IllegalArgumentException("Root folder is null. Usage java -jar search.jar ROOT_FOLDER.");
+        Path start = Paths.get(args[0]);
+        if (!start.toFile().exists()) {
+            throw new IllegalArgumentException("Root folder doesn't exist");
         }
-        if (args.length < 2 || args[1] == null) {
-            throw new IllegalArgumentException("Search condition is null");
+        if (!args[1].startsWith(".")) {
+            args[1] = "." + args[1];
         }
         Search searcher = new Search();
-        Path start = Paths.get(args[0]);
-        searcher.search(start, p -> p.toFile().getName().startsWith(args[1])).forEach(System.out::println);
+        searcher.search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public List<Path> search(Path root, Predicate<Path> condition) throws IOException {
