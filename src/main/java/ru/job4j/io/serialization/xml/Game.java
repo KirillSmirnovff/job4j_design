3b4c00a6970d345eb0c3712494495019e766bfa3
KirillSmirnovff <smirnovk.1990@gmail.com>
2022-02-13 13:32:33 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/io/serialization/xml/Game.java
+++ src/main/java/ru/job4j/io/serialization/xml/Game.java
@@ -1,13 +1,23 @@
 package ru.job4j.io.serialization.xml;
 
+import javax.xml.bind.annotation.*;
 import java.util.Arrays;
 
+@XmlRootElement(name = "game")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Game {
-    private final String name;
-    private final boolean pg13;
-    private final int cost;
-    private final String[] genre;
-    private final Publisher publisher;
+    @XmlAttribute
+    private String name;
+    @XmlAttribute
+    private boolean pg13;
+    @XmlAttribute
+    private int cost;
+    @XmlElementWrapper(name = "genres")
+    @XmlElement(name = "genre")
+    private String[] genre;
+    private Publisher publisher;
+
+    public Game() { }
 
     public Game(String name, boolean pg13, int cost, String[] genre, Publisher publisher) {
         this.name = name;
--- src/main/java/ru/job4j/io/serialization/xml/Main.java
+++ src/main/java/ru/job4j/io/serialization/xml/Main.java
@@ -1,11 +1,28 @@
 package ru.job4j.io.serialization.xml;
 
-import ru.job4j.io.serialization.json.Game;
-import ru.job4j.io.serialization.json.Publisher;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        final ru.job4j.io.serialization.json.Game game = new Game("The Witcher 3", false, 1499, new String[]{"Action", "RPG"},
+    public static void main(String[] args) throws Exception {
+        final Game game = new Game("The Witcher 3", false, 1499, new String[]{"Action", "RPG"},
                 new Publisher("CD Projekt RED"));
+        JAXBContext context = JAXBContext.newInstance(Game.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(game, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Game result = (Game) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
     }
 }
