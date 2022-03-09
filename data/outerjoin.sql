a3b34b1f99efcf655ee5f14953da70243501c0be
KirillSmirnovff <smirnovk.1990@gmail.com>
2022-03-09 13:33:44 +0300

    0. Join [#6862 #272613]

--- data/outerjoin.sql
+++ data/outerjoin.sql
@@ -9,21 +9,33 @@ create table employers(
 	dep_id int references departments(id)
 );
 
-insert into departments(name) values ('first'),('second'),('third');
-insert into employers(name,dep_id) values ('James',1),('Scott',1),('Jack',1),('John',2),('Jacob',2);
+insert into departments(name)
+values ('first'),('second'),('third');
 
-select * from departments d left join employers e on d.id = e.dep_id;
+insert into employers(name,dep_id)
+values ('James',1),('Scott',1),('Jack',1),('John',2),('Jacob',2);
 
-select * from employers e right join departments d on e.dep_id = d.id;
+select * from departments d
+left join employers e on d.id = e.dep_id;
 
-select * from departments d full join employers e on d.id = e.dep_id;
+select * from employers e
+right join departments d on e.dep_id = d.id;
 
-select * from departments d cross join employers e;
+select * from departments d f
+ull join employers e on d.id = e.dep_id;
 
-select * from departments d left join employers e on e.dep_id = d.id where e.dep_id is null;
+select * from departments d
+cross join employers e;
 
-select * from departments d left join employers e on d.id = e.dep_id where e.dep_id is not null;
-select * from departments d right join employers e on e.dep_id = d.id;
+select * from departments d
+left join employers e on e.dep_id = d.id
+where e.dep_id is null;
+
+select * from departments d
+left join employers e on d.id = e.dep_id
+where e.dep_id is not null;
+select * from departments d
+right join employers e on e.dep_id = d.id;
 
 
 
@@ -33,7 +45,11 @@ create table teens(
 	gender varchar(255)
 );
 
-insert into teens(name,gender) values ('Mike','Male'),('John','Male'),('james','Male'),('Amanda','Female'),('Monica','Female'),('Bella','Female');
+insert into teens(name,gender)
+values ('Mike','Male'),('John','Male'),('james','Male'),
+('Amanda','Female'),('Monica','Female'),('Bella','Female');
 
-select t1.name, t2.name from teens t1 cross join teens t2 where t1.gender != t2.gender;
+select t1.name, t2.name from teens t1
+cross join teens t2
+where t1.gender != t2.gender;
 
